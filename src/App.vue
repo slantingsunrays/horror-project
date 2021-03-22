c32c9ecb1c7a891e1e17524f4f362e373b384a1b
You-you <shinyou_hou@hotmail.com>
2021-03-22 18:55:36 -0500

    start

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <Text/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Text from './components/Text.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Text
   }
 }
 </script>
---
+++ src/components/Text.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+    <h1>Adventure Area</h1>
+    <h2>{{currentLocation.name}}</h2>
+    <p>{{currentLocation.text}}</p>
+    <input type="text" v-on:keydown.enter="performAction" v-model="userInput">
+</div>
+  
+</template>
+
+<script>
+export default {
+name:'mainComponent',
+    data(){
+        return{
+            currentLocation:{ 
+                name:null,
+                text:null
+            },
+            locations:{
+                one:{
+                    name:"Jetty", 
+                    text:"You wake up and find yourself on an old jetty.",
+                    north:'two', 
+                    east:'null',
+                    west:'null',
+                    south:'null'
+
+                }, 
+                two:{
+                    name:"Shore", 
+                    text:"You find your way to the shore",
+                    north:'null', 
+                    east:'null',
+                    west:'null',
+                    south:'null'
+                }                
+
+            },
+            userInput:null 
+        }
+    },
+    created(){
+        this.currentLocation=this.locations['one']; 
+    },
+    methods:{
+        performAction(){
+            console.log(this.userInput)
+            switch(this.userInput){
+                case 'north':
+                    if(this.currentLocation.north!==null){
+                        this.currentLocation=this.locations[this.currentLocation.north]; 
+                    }
+
+            }
+        }
+        
+    }
+
+}
+</script>
+
+<style  scoped>
+
+</style>>
+
